31d9fe9e4cee1ca2659e5de0dba790d8163fa177
sameer2006-s <sameer.ahmed20062006@gmail.com>
2024-12-21 21:16:16 +0200

    a7a nek

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  build: {
-    outDir: "./dist", // Ensure this matches tsconfig's `outDir`
-    sourcemap: true, // Useful for debugging issues on Vercel
-  },
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
   },
-})
+});
